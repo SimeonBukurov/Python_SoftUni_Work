9cad0db7b4877111e5e210bb50bfd861e661f904
Simeon Bukurov <71982116+SwagerStorm@users.noreply.github.com>
2022-03-21 21:22:12 +0200

    Add files via upload

---
+++ Python_Basic/Conditional_Statement_Advanced/Lab/Exercise2.Breakday_or_Workingday.py
@@ -0,0 +1,17 @@
+days = str(input())
+if days == "Monday":
+    print("Working day")
+elif days == "Tuesday":
+    print("Working day")
+elif days == "Wednesday":
+    print("Working day")
+elif days == "Thursday":
+    print("Working day")
+elif days == "Friday":
+    print("Working day")
+elif days == "Saturday":
+    print("Weekend")
+elif days == "Sunday":
+    print("Weekend")
+else:
+    print("Error")
